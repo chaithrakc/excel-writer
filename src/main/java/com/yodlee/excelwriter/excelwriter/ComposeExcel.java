c41ca5ccf402d11fafa4851ca5c7fb2d10d31c26
Chaithra KC <ccheluvaiah@yodlee.com>
2019-12-24 13:23:44 +0530

    refactoring the code

--- src/main/java/com/yodlee/excelwriter/excelwriter/ComposeExcel.java
+++ src/main/java/com/yodlee/excelwriter/excelwriter/ComposeExcel.java
@@ -6,7 +6,6 @@ import org.apache.log4j.Logger;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,35 +15,30 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class ComposeExcel<ExcelVo> {
+public class ComposeExcel<T> {
     private final static Logger LOGGER = Logger.getLogger(ComposeExcel.class);
     private SXSSFWorkbook workbook;
     private SXSSFSheet sheet;
     private File excelFile;
-    private Class<ExcelVo> excelVo;
+    private Class<T> excelVo;
     private Map<String,Integer> headerRowMapper = new HashMap<>();
     private AtomicInteger rowNum = new AtomicInteger();
-
-    private ComposeExcel(File excelFile, Class<ExcelVo> excelVoClass) throws IOException {
+    private ComposeExcel(File excelFile, Class<T> excelVoClass) throws IOException {
         isWriteableFile(excelFile);
         this.excelVo = excelVoClass;
         workbook = new SXSSFWorkbook(SXSSFWorkbook.DEFAULT_WINDOW_SIZE);
         sheet = workbook.createSheet(this.excelVo.getSimpleName());
-
     }
-
-    public ComposeExcel(String excelFilePath, Class<ExcelVo> excelVoClass) throws IOException{
+    public ComposeExcel(String excelFilePath, Class<T> excelVoClass) throws IOException{
         this(new File(excelFilePath), excelVoClass);
     }
-
-    public void renderData(List<ExcelVo> excelVoList) throws ExcelWriterException {
+    public void renderData(List<T> tList) throws ExcelWriterException {
         getExcelVoFields();
         createHeaderRow();
-        generateExcelRows(excelVoList);
+        generateExcelRows(tList);
     }
-
-    private void generateExcelRows(List<ExcelVo> excelVoList) throws ExcelWriterException {
-        for (ExcelVo vo : excelVoList) {
+    private void generateExcelRows(List<T> tList) throws ExcelWriterException {
+        for (T vo : tList) {
             Row row = sheet.createRow(rowNum.incrementAndGet());
             try {
                 populateExcelRows(row, vo);
@@ -55,20 +49,17 @@ public class ComposeExcel<ExcelVo> {
         }
         LOGGER.info("rows written "+rowNum);
     }
-
     private void getExcelVoFields() {
         Field[] declaredFields = excelVo.getDeclaredFields();
         AtomicInteger columnNum = new AtomicInteger(0);
         Arrays.stream(declaredFields).forEach(field -> headerRowMapper.put(ExcelHelper.capitalizeInitialLetter(field.getName()), columnNum.getAndIncrement()));
     }
-
     private void createHeaderRow() {
         Row headerRow = sheet.createRow(0);
         headerRowMapper.forEach((headerName, columnNum) ->
                 formatHeaderRow(createCell(headerRow, columnNum, headerName))
         );
     }
-
     private void formatHeaderRow(Cell headerCell) {
         CellStyle style = workbook.createCellStyle();
         Font font = workbook.createFont();
@@ -78,7 +69,6 @@ public class ComposeExcel<ExcelVo> {
         style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         headerCell.setCellStyle(style);
     }
-
     public void generateExcelFile() throws ExcelWriterException, IOException {
         FileOutputStream fileOutputStream = null;
         try {
@@ -93,8 +83,7 @@ public class ComposeExcel<ExcelVo> {
             workbook.close();
         }
     }
-
-    private void populateExcelRows(Row row, ExcelVo excelVo) throws ExcelWriterException {
+    private void populateExcelRows(Row row, T t) throws ExcelWriterException {
         AtomicReference<Object> cellValue = new AtomicReference<>();
         String fieldName;
         Integer columnNum;
@@ -102,21 +91,19 @@ public class ComposeExcel<ExcelVo> {
             fieldName = entry.getKey();
             columnNum = entry.getValue();
             try {
-                Method method = excelVo.getClass().getMethod("get".concat(fieldName));
-                cellValue.set(method.invoke(excelVo));
+                Method method = t.getClass().getMethod("get".concat(fieldName));
+                cellValue.set(method.invoke(t));
                 createCell(row, columnNum, cellValue.get());
             } catch (Exception ex) {
                 throw new ExcelWriterException("unable to write data for columnNum" + columnNum + " fieldName " + fieldName);
             }
         }
     }
-
     private void isWriteableFile(File excelFile) throws IOException {
         if (!excelFile.canWrite())
             throw new IOException("Unable to write " + excelFile.getAbsolutePath());
         this.excelFile = excelFile;
     }
-
     private Cell createCell(Row row, Integer columnNumber, Object cellValue) {
         Cell cell = row.createCell(columnNumber);
         cell.setCellValue(cellValue != null ? cellValue.toString().trim() : ExcelHelper.STRING_EMPTY);
